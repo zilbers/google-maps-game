09d384edd06453e13d8b9a9ce3f04f0f0b8e3603
Omri Zilberstein <67857576+zilbers@users.noreply.github.com>
2020-10-16 23:02:29 +0300

    Added side menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import WrappedMap from './components/Map';
 import SideMenu from './components/SideMenu';
 import './App.css';
--- client/src/components/SideMenu.js
+++ client/src/components/SideMenu.js
@@ -1,23 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style/SideMenu.css';
 
 function SideMenu() {
+  const [visible, setVisible] = useState(false);
+
+  let visibility = 'hide';
+
+  if (visible) {
+    visibility = 'show';
+  }
+
+  const toggleMenu = () => {
+    setVisible(!visible);
+  };
+
+  const handleMouseDown = (e) => {
+    toggleMenu();
+
+    console.log('clicked');
+    e.stopPropagation();
+  };
   return (
     <div id='side-menu'>
-      <div>
-        <div>
-          <p>Can you spot the item that doesn't belong?</p>
-          <ul>
-            <li>Lorem</li>
-            <li>Ipsum</li>
-            <li>Dolor</li>
-            <li>Sit</li>
-            <li>Bumblebees</li>
-            <li>Aenean</li>
-            <li>Consectetur</li>
-          </ul>
+      {visible ? (
+        <div
+          id='flyoutMenu'
+          onMouseDown={handleMouseDown}
+          className={visibility}
+        >
+          <h2>
+            <a href='#'>Home</a>
+          </h2>
+          <h2>
+            <a href='#'>About</a>
+          </h2>
+          <h2>
+            <a href='#'>Contact</a>
+          </h2>
+          <h2>
+            <a href='#'>Search</a>
+          </h2>
         </div>
-      </div>
+      ) : (
+        <button id='roundButton' onMouseDown={handleMouseDown}></button>
+      )}
     </div>
   );
 }
